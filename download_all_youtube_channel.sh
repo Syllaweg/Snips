a83d83eacb6f7ca82d4e4151a24f5ee860c02efc
Syllaweg <sylvere68@hotmail.fr>
2021-09-07 19:23:09 +0200

    add comment

--- download_all_youtube_channel.sh
+++ download_all_youtube_channel.sh
@@ -1,3 +1,5 @@
+# Script pour telecharger toutes les videos d'une chaine youtube avec youtube-dl
+
 # Installe youtube-dl, peut etre installer avec pip 
 # sudo snap install youtube-dl
 
@@ -8,19 +10,19 @@ youtube-dl -f best -ciw -o "%(title)s.%(ext)s" -v <channel-url>
 # Commandes utilise
  
 # -f, --format FORMAT
-    video format code. The special name "best" will pick the best quality.
+   # video format code. The special name "best" will pick the best quality.
 
 # -c, --continue                   
-    force resume of partially downloaded files
+   # force resume of partially downloaded files
 
 # -i, --ignore-errors              
-    continue on download errors, for example to skip unavailable videos in a channel 
+   # continue on download errors, for example to skip unavailable videos in a channel 
 
 # -w, --no-overwrites
-    do not overwrite files
+   # do not overwrite files
 
 # -o, --output
     Output filename template, this example functions similarly to the old --title option
 
 # -v, --verbose
-    print various debugging information
+   # print various debugging information
